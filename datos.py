3e28f76b9d9449bb9b3c645a9de3fd7f7061b0fc
dpenad3 <dpenad3@ucentral.edu.co>
2020-05-06 13:35:11 -0500

    Primeras gráficas

---
+++ datos.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import os  # Módulo da acceso a operaciones a nivel sistema operativo
+
+# Leer el documento datos.csv
+datos = pd.read_csv(os.path.join('data/COVID-19_Colombia.csv'))
+
+# Datos de la columna Atención
+datosA = datos['atención']
+
+# Datos de la columna Fecha de notificación
+datosF = datos['Fecha de notificación']
+
+
+f = 0
+r = 0
+i = 0
+
+for estado in datosA:
+    if estado == 'Fallecido':
+        f = f+1
+    elif estado == 'Recuperado':
+        r = r+1
+    else:
+        i = i+1
+        
+N = r+f+i
+
+
+# Datos por Género
+femenino = 0
+masculino = 0
+otro = 0
+
+for genero in datos['Sexo']:
+    if genero == 'F':
+        femenino += 1
+    elif genero == 'M':
+        masculino += 1
+    else:
+        otro += 1
---
+++ main.py
@@ -0,0 +1,137 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import datos
+
+# import plotly.plotly as py
+
+# from plotly.graph_objs import *
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='COVID-19 Colombia',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Un dashboard del COVID-19', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='bar IRF',
+        figure={
+            'data': [
+                {'x': ['Infectados', 'Recuperados', 'Fallecidos', 'Total'], 'y': [datos.i, datos.r, datos.f, datos.N],
+                 'type': 'bar', 'name': 'IRFT'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    ),
+
+    html.Table([
+        html.Thead(
+            html.Tr([html.Th('Infectados', style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+             html.Th('Recuperados', style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+            html.Th('Fallecidos', style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+            html.Th('Total', style={
+                'textAlign': 'center',
+                'color': colors['text']
+            })])
+        ),
+        html.Tbody([
+            html.Tr([html.Td(datos.i, style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+             html.Td(datos.r, style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+             html.Td(datos.f, style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+             html.Td(datos.N, style={
+                'textAlign': 'center',
+                'color': colors['text']
+            })])
+        ]),
+    ]),
+
+    html.Div(children='COVID-19 por Género', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='bar genero',
+        figure={
+            'data': [
+                {'x': ['Mujeres', 'Hombres', 'Otro'], 'y': [datos.femenino, datos.masculino, datos.otro],
+                 'type': 'bar', 'name': 'Género'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    ),
+
+    html.Div(children='Fecha-Estado', style={
+            'textAlign': 'center',
+            'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='fech-est',
+        figure={
+            'data': [
+                {'x': datos.datos['Fecha de notificación'], 'y': datos.datosA,
+                 'type': 'bar', 'name': 'Estado'},
+            ],
+            'layout': {
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    ),
+
+])
+
+# Cargar página
+if __name__ == '__main__':
+    app.run_server(debug=True)
